9dddf3df0c72ec5a45490c6129168a1ae70e5f47
Joshua Slycord <jslycord@uncc.edu>
2018-02-13 13:32:38 -0500

    More function definitions filled out in compute_node.cc Changed the function prototypes in operations.cc and operations.hh Parameters storage structure extended for new variables, current version of struct has gotten messy, I have a planned remodel that can be quickly implemented but have learned it will be most effective after completition of the compute_node.cc
    Change-Id: I6f4831fd4723860f2b2debea0ed842d9455ddc9e

--- src/hwacc/LLVMRead/src/operations.hh
+++ src/hwacc/LLVMRead/src/operations.hh
@@ -8,11 +8,11 @@
 #include <sstream>
 #include <vector>
 #include <cmath>
-#include "base/types.hh"
+//#include "base/types.hh"
 
 #define DEFAULT -1
 
-struct parameters{
+struct Parameters{
 	std::string returnReg, operand1, operand2, dataType;
 	
 	
@@ -21,87 +21,87 @@ struct parameters{
 class Operations {
   public:  
     /* LLVM Terminator Instructions */
-     void llvm_move(struct parameters);
-     void llvm_ret(struct parameters);
-     std::string llvm_br(struct parameters);
-     void llvm_switch(struct parameters);
-     void llvm_indirectbr(struct parameters);
-     void llvm_invoke(struct parameters);
-     void llvm_resume(struct parameters);
-     void llvm_unreachable(struct parameters);
+     void llvm_move(Parameters &&parameters);
+     void llvm_ret(Parameters &parameters);
+     std::string llvm_br(Parameters &parameters);
+     void llvm_switch(Parameters &parameters);
+     void llvm_indirectbr(Parameters &parameters);
+     void llvm_invoke(Parameters &parameters);
+     void llvm_resume(Parameters &parameters);
+     void llvm_unreachable(Parameters &parameters);
 
     /* LLVM Binary Operations*/
-     void llvm_add(struct parameters);
-     void llvm_fadd(struct parameters);
-     void llvm_sub(struct parameters);
-     void llvm_fsub(struct parameters);
-     void llvm_mul(struct parameters);
-     void llvm_fmul(struct parameters);
-     void llvm_udiv(struct parameters);
-     void llvm_sdiv(struct parameters);
-     void llvm_fdiv(struct parameters);
-     void llvm_urem(struct parameters);
-     void llvm_srem(struct parameters);
-     void llvm_frem(struct parameters);
+     void llvm_add(Parameters &parameters);
+     void llvm_fadd(Parameters &parameters);
+     void llvm_sub(Parameters &parameters);
+     void llvm_fsub(Parameters &parameters);
+     void llvm_mul(Parameters &parameters);
+     void llvm_fmul(Parameters &parameters);
+     void llvm_udiv(Parameters &parameters);
+     void llvm_sdiv(Parameters &parameters);
+     void llvm_fdiv(Parameters &parameters);
+     void llvm_urem(Parameters &parameters);
+     void llvm_srem(Parameters &parameters);
+     void llvm_frem(Parameters &parameters);
 
     /* LLVM Bitwise Binary Operations */
-     void llvm_shl(struct parameters);
-     void llvm_lshr(struct parameters);
-     void llvm_ashr(struct parameters);
-     void llvm_and(struct parameters);
-     void llvm_or(struct parameters);
-     void llvm_xor(struct parameters);
+     void llvm_shl(Parameters &parameters);
+     void llvm_lshr(Parameters &parameters);
+     void llvm_ashr(Parameters &parameters);
+     void llvm_and(Parameters &parameters);
+     void llvm_or(Parameters &parameters);
+     void llvm_xor(Parameters &parameters);
 
     /* LLVM Memory Access Operations */
-     void llvm_alloca(struct parameters);
-     void llvm_load(struct parameters);
-     void llvm_store(struct parameters);
-     void llvm_getelementptr(struct parameters);
-     void llvm_fence(struct parameters);
-     void llvm_cmpxchg(struct parameters);
-     void llvm_atomicrmw(struct parameters);
+     void llvm_alloca(Parameters &parameters);
+     void llvm_load(Parameters &parameters);
+     void llvm_store(Parameters &parameters);
+     void llvm_getelementptr(Parameters &parameters);
+     void llvm_fence(Parameters &parameters);
+     void llvm_cmpxchg(Parameters &parameters);
+     void llvm_atomicrmw(Parameters &parameters);
 
     /* LLVM Conversion Operations */
-     void llvm_trunc(struct parameters);
-     void llvm_zext(struct parameters);
-     void llvm_sext(struct parameters);
-     void llvm_fptoui(struct parameters);
-     void llvm_fptosi(struct parameters);
-     void llvm_uitofp(struct parameters);
-     void llvm_sitofp(struct parameters);
-     void llvm_fptrunc(struct parameters);
-     void llvm_fpext(struct parameters);
-     void llvm_ptrtoint(struct parameters);
-     void llvm_inttoptr(struct parameters);
-     void llvm_bitcast(struct parameters);
-     void llvm_addrspacecast(struct parameters);
+     void llvm_trunc(Parameters &parameters);
+     void llvm_zext(Parameters &parameters);
+     void llvm_sext(Parameters &parameters);
+     void llvm_fptoui(Parameters &parameters);
+     void llvm_fptosi(Parameters &parameters);
+     void llvm_uitofp(Parameters &parameters);
+     void llvm_sitofp(Parameters &parameters);
+     void llvm_fptrunc(Parameters &parameters);
+     void llvm_fpext(Parameters &parameters);
+     void llvm_ptrtoint(Parameters &parameters);
+     void llvm_inttoptr(Parameters &parameters);
+     void llvm_bitcast(Parameters &parameters);
+     void llvm_addrspacecast(Parameters &parameters);
 
     /* LLVM Control Operations */
-     void llvm_icmp(struct parameters);
-     void llvm_fcmp(struct parameters);
-     void llvm_phi(struct parameters, std::string prevBB);
-     void llvm_call(struct parameters);
-     void llvm_select(struct parameters);
-     void llvm_vaarg(struct parameters);
-     void llvm_landingpad(struct parameters);
+     void llvm_icmp(Parameters &parameters);
+     void llvm_fcmp(Parameters &parameters);
+     void llvm_phi(Parameters &parameters, std::string prevBB);
+     void llvm_call(Parameters &parameters);
+     void llvm_select(Parameters &parameters);
+     void llvm_vaarg(Parameters &parameters);
+     void llvm_landingpad(Parameters &parameters);
 
     /* LLVM Vector Operations*/
-     void llvm_extractelement(struct parameters);
-     void llvm_insertelement(struct parameters);
-     void llvm_shufflevector(struct parameters);
+     void llvm_extractelement(Parameters &parameters);
+     void llvm_insertelement(Parameters &parameters);
+     void llvm_shufflevector(Parameters &parameters);
 
     /* LLVM Aggregate Operations */
-     void llvm_extractvalue(struct parameters);
-     void llvm_insertvalue(struct parameters);
+     void llvm_extractvalue(Parameters &parameters);
+     void llvm_insertvalue(Parameters &parameters);
 
     /* Custome Operations (Not implemented yet) */
-     void llvm_dmafence(struct parameters);
-     void llvm_dmastore(struct parameters);
-     void llvm_dmaload(struct parameters);
-     void llvm_indexadd(struct parameters);
-     void llvm_silentstore(struct parameters);
-     void llvm_sine(struct parameters);
-     void llvm_cosine(struct parameters);
+     void llvm_dmafence(Parameters &parameters);
+     void llvm_dmastore(Parameters &parameters);
+     void llvm_dmaload(Parameters &parameters);
+     void llvm_indexadd(Parameters &parameters);
+     void llvm_silentstore(Parameters &parameters);
+     void llvm_sine(Parameters &parameters);
+     void llvm_cosine(Parameters &parameters);
 };
 
 #endif //__OPERATIONS_HH__
