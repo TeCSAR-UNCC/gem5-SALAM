2e35d52d9c1ea8d1b630bc6e03142ba666cca05f
Samuel Rogers <sroger48@uncc.edu>
2018-04-16 16:30:59 -0400

    Updated Baremetal Files
    Change-Id: I074e561a04adf8f7f79e5ce6b4899ee464f9de8c

---
+++ baremetal/VAdd/vadd.h
@@ -0,0 +1,33 @@
+#define common_val *(unsigned *)0x8f000000
+#define acc        *(char *)0x2f000000
+#define val_a      *(int *)0x2f000001
+#define val_b      *(int *)0x2f000005
+#define val_c      *(int *)0x2f000009
+
+typedef struct {
+    int * a;
+    int * b;
+    int * c;
+    int * check;
+    int length;
+} vadd_struct;
+
+void checkData(vadd_struct * vas) {
+    int i;
+    for (i = 0; i < vas->length; i++) {
+        if (vas->c[i] != vas->check[i]) {
+            printf("Check Failed\n");
+            return;
+        }
+    }
+    printf("Check Passed\n");
+}
+
+void genData(vadd_struct * vas) {
+    int i;
+    for (i = 0; i < vas->length; i++) {
+        vas->a[i] = i;
+        vas->b[i] = vas->length - i;
+        vas->check[i] = vas->a[i] + vas->b[i];
+    }
+}
