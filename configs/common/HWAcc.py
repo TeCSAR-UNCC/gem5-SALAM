dcbab380f88092051d48088bcc9ac5adf00c4ca7
Samuel Rogers <sroger48@uncc.edu>
2018-10-09 16:48:32 -0400

    Updated AccCluster to simplify system cosntruction
    Change-Id: I3e43c2520100ed23f882a6a2df04d1466b3a6c8d

--- configs/common/HWAcc.py
+++ configs/common/HWAcc.py
@@ -3,64 +3,35 @@ from m5.objects import *
 from m5.util import *
 
 def makeHWAcc(options, system):
+    # Construct our Accelerator Cluster and add a SPM of size 128kB
     system.acc_cluster = AccCluster()
-    system.acc_cluster.bus = NoncoherentXBar(width=16, frontend_latency=1, forward_latency=0, response_latency=1)
+    system.acc_cluster._attach_bridges(system, AddrRange(0x2f000000, 0x7fffffff), [AddrRange(0x00000000, 0x2effffff), AddrRange(0x80000000, 0xffffffff)])
+    system.acc_cluster._add_spm(AddrRange(0x2f100000, size='128kB'), '2ns')
+    system.acc_cluster._connect_caches(system, options, '1kB')
+
+    # Add an accelerator to the cluster and connect it
     system.acc_cluster.acc = CommInterface(pio_addr=0x2f000000, pio_size=64, gic=system.realview.gic)
-    system.acc_cluster.acc.pio = system.acc_cluster.bus.master
     system.acc_cluster.acc.flags_size = 1
     system.acc_cluster.acc.config_size = 0
-    system.acc_cluster.cache_bus = NoncoherentXBar(width=16, frontend_latency=1, forward_latency=0, response_latency=1)
-    system.acc_cluster.acc.dram_side = system.acc_cluster.cache_bus.slave
-    if options.acc_cache:
-        system.acc_cluster_cache = Cache(assoc=4, tag_latency=1, data_latency=1, response_latency=1, mshrs=16, size='1kB', tgts_per_mshr=20)
-#        system.acc_cluster_cache.prefetch_on_access = 'true'
-#        system.acc_cluster_cache.prefetcher = TaggedPrefetcher(degree=8, latency=1)
-        if options.l2cache:
-            system.acc_cluster_cache.mem_side = system.tol2bus.slave
-        else:
-            system.acc_cluster_cache.mem_side = system.membus.slave
-        system.acc_cluster.cache_bus.master = system.acc_cluster_cache.cpu_side
-    else:
-        if options.l2cache:
-            system.acc_cluster.cache_bus.master = system.tol2bus.slave
-        else:
-            system.acc_cluster.cache_bus.master = system.membus.slave
-    system.acc_cluster.acc.spm_side = system.acc_cluster.bus.slave
     system.acc_cluster.acc.dram_ranges = [AddrRange(0x80000000, '4GB')]
     system.acc_cluster.llvm_interface = LLVMInterface()
     system.acc_cluster.llvm_interface.in_file = options.accpath + "/" + options.accbench + "/bench/" + options.accbench + ".ll"
-
-
     system.acc_cluster.acc.int_num = 68
     system.acc_cluster.acc.clock_period = 10
+    system.acc_cluster._connect_hwacc(system.acc_cluster.acc)
 
-    system.mem_to_hwacc = Bridge(delay='50ns')
-    system.mem_to_hwacc.ranges = AddrRange(0x2f000000, 0x7fffffff)
-    system.mem_to_hwacc.master = system.acc_cluster.bus.slave
-    system.mem_to_hwacc.slave = system.membus.master
-
-    system.hwacc_to_mem = Bridge(delay='50ns')
-    system.hwacc_to_mem.ranges = AddrRange(0x80000000, 0xffffffff)
-    system.hwacc_to_mem.master = system.membus.slave
-    system.hwacc_to_mem.slave = system.acc_cluster.bus.master
-
-    system.acc_cluster.spm = SimpleMemory(range=AddrRange(0x2f100000, size='128kB'),
-                                  conf_table_reported=False, latency='2ns')
-    system.acc_cluster.spm.port = system.acc_cluster.bus.master
-
+    # Add DMA devices to the cluster and connect them
     system.acc_cluster.dma = NoncoherentDma(pio_addr=0x2ff00000, pio_size=24, gic=system.realview.gic, max_pending=32)
-    system.acc_cluster.dma.pio = system.acc_cluster.bus.master
-    system.acc_cluster.dma.dma = system.membus.slave
+    system.acc_cluster._connect_dma(system, system.acc_cluster.dma)
 
     system.acc_cluster.stream_dma_0 = StreamDma(pio_addr=0x2ff10000, pio_size=40, gic=system.realview.gic, max_pending=32)
     system.acc_cluster.stream_dma_0.pio_delay = '1ns'
-    system.acc_cluster.stream_dma_0.pio = system.acc_cluster.bus.master
-    system.acc_cluster.stream_dma_0.dma = system.membus.slave
     system.acc_cluster.stream_dma_0.rd_int = 210
     system.acc_cluster.stream_dma_0.wr_int = 211
+    system.acc_cluster._connect_dma(system, system.acc_cluster.stream_dma_0)
+
     system.acc_cluster.stream_dma_1 = StreamDma(pio_addr=0x2ff20000, pio_size=40, gic=system.realview.gic, max_pending=32)
     system.acc_cluster.stream_dma_1.pio_delay = '1ns'
-    system.acc_cluster.stream_dma_1.pio = system.acc_cluster.bus.master
-    system.acc_cluster.stream_dma_1.dma = system.membus.slave
     system.acc_cluster.stream_dma_1.rd_int = 212
     system.acc_cluster.stream_dma_1.wr_int = 213
+    system.acc_cluster._connect_dma(system, system.acc_cluster.stream_dma_1)
--- src/hwacc/AccCluster.py
+++ src/hwacc/AccCluster.py
@@ -4,9 +4,54 @@ from Device import BasicPioDevice, PioDevice, IsaFake, BadAddr, DmaDevice
 from Platform import Platform
 from SimpleMemory import SimpleMemory
 from SubSystem import SubSystem
-
+from XBar import NoncoherentXBar
+from Bridge import Bridge
+from Cache import Cache
+from CommInterface import CommInterface
+from NoncoherentDma import NoncoherentDma
+from StreamDma import StreamDma
 
 class AccCluster(Platform):
     type = 'AccCluster'
     cxx_header = "hwacc/acc_cluster.hh"
     system = Param.System(Parent.any, "system")
+
+    local_bus = NoncoherentXBar(width=16, frontend_latency=1, forward_latency=0, response_latency=1)
+    cache_bus = NoncoherentXBar(width=16, frontend_latency=1, forward_latency=0, response_latency=1)
+
+    def _add_spm(self, spm_range, spm_latency):
+        self.spm = SimpleMemory(range=spm_range, conf_table_reported=False, latency=spm_latency)
+        self.spm.port = self.local_bus.master
+
+    def _attach_bridges(self, system, mem_range, ext_ranges):
+        system.mem_to_hwacc = Bridge(delay='1ns', ranges = mem_range)
+        system.mem_to_hwacc.master = self.local_bus.slave
+        system.mem_to_hwacc.slave = system.membus.master
+
+        system.hwacc_to_mem = Bridge(delay='1ns', ranges = ext_ranges)
+        system.hwacc_to_mem.master = system.membus.slave
+        system.hwacc_to_mem.slave = self.local_bus.master
+
+    def _connect_hwacc(self, hwacc):
+        hwacc.pio = self.local_bus.master
+        hwacc.dram_side = self.cache_bus.slave
+        hwacc.spm_side = self.local_bus.slave
+
+    def _connect_caches(self, system, options, cache_size):
+        if options.acc_cache:
+            self.cache = Cache(assoc=4, tag_latency=1, data_latency=1, response_latency=1, mshrs=16, size=cache_size, tgts_per_mshr=20)
+
+            if options.l2cache:
+                self.cache.mem_side = system.tol2bus.slave
+            else:
+                self.cache.mem_side = system.membus.slave
+            self.cache_bus.master = self.cache.cpu_side
+        else:
+            if options.l2cache:
+                self.cache_bus.master = system.tol2bus.slave
+            else:
+                self.cache_bus.master = system.membus.slave
+
+    def _connect_dma(self, system, dma):
+        dma.pio = self.local_bus.master
+        dma.dma = system.membus.slave
