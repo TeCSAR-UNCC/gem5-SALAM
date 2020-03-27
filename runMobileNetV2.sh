dfac7db5401c1fff82574ecb08ee07f17784113c
Sam Rogers <sroger48@uncc.edu>
2020-03-27 11:31:11 -0400

    Added debug option to runMobileNetV2.sh

--- runMobileNetV2.sh
+++ runMobileNetV2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 FLAGS=IOAcc,LLVMInterface,CommInterface,NoncoherentDma,ComputeUnit
 
+echo "$1"
+if [ "$1" == "debug" ]; then
+    BINARY="ddd --gdb --args build/ARM/gem5.debug"
+else
+    BINARY=build/ARM/gem5.opt
+fi
+
 KERNEL=$M5_PATH/benchmarks/mobilenetv2/sw/main.elf
 SYS_OPTS="--mem-size=4GB \
           --kernel=$KERNEL \
@@ -12,8 +19,8 @@ CACHE_OPTS="" #"--caches --acc_cache"
 # Script to start up full system simulation
 # --debug-flags=$FLAGS
 echo "Running Single Accelerator Configuration"
-build/ARM/gem5.opt --debug-flags=$FLAGS --outdir=BM_ARM_OUT/mobilenetv2 configs/SALAM/fs_mobilenetv2.py \
-$SYS_OPTS --accpath=$M5_PATH/benchmarks $CACHE_OPTS #> BM_ARM_OUT/$BENCH/debug_trace.txt
+$BINARY --debug-flags=$FLAGS --outdir=BM_ARM_OUT/mobilenetv2 configs/SALAM/fs_mobilenetv2.py \
+$SYS_OPTS --accpath=$M5_PATH/benchmarks $CACHE_OPTS
 
 # Debug Flags List
 #
