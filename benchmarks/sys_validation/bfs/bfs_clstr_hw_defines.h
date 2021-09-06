0dc4cde42591854430fcb4aea307bfb96874839c
Sam Rogers <sroger48@uncc.edu>
2021-09-06 14:11:26 -0400

    Added new configs for BFS and FFT for System Validation versions

---
+++ benchmarks/sys_validation/bfs/bfs_clstr_hw_defines.h
@@ -0,0 +1,16 @@
+//BEGIN GENERATED CODE
+//Cluster: BFS
+//NonCoherentDMA
+#define DMA_Flags 0x10020000
+#define DMA_RdAddr 0x10020001
+#define DMA_WrAddr 0x10020009
+#define DMA_CopyLen 0x10020011
+//Accelerator: TOP
+#define TOP 0x10020040
+//Accelerator: BFS
+#define BFS 0x10020080
+#define NODES 0x100200c0
+#define EDGES 0x10020900
+#define LEVELS 0x10024940
+#define LEVELCOUNTS 0x10024a80
+//END GENERATED CODE
---
+++ benchmarks/sys_validation/fft/fft_clstr_hw_defines.h
@@ -0,0 +1,16 @@
+//BEGIN GENERATED CODE
+//Cluster: FFT
+//NonCoherentDMA
+#define DMA_Flags 0x10020000
+#define DMA_RdAddr 0x10020001
+#define DMA_WrAddr 0x10020009
+#define DMA_CopyLen 0x10020011
+//Accelerator: TOP
+#define TOP 0x10020040
+//Accelerator: FFT
+#define FFT 0x10020080
+#define REAL 0x100200c0
+#define IMG 0x10022100
+#define REALTWID 0x10024140
+#define IMGTWID 0x10025180
+//END GENERATED CODE
