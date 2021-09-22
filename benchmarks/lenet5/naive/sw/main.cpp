03920c15041dcc12e1fbe71408b3c50d0991c8d3
Zephan Spencer <number45zephan@gmail.com>
2021-09-22 19:48:15 +0000

    Lenet runs, fixed stupid issues that were stupid

--- benchmarks/lenet5/naive/sw/main.cpp
+++ benchmarks/lenet5/naive/sw/main.cpp
@@ -6,8 +6,8 @@
 
 int main(void) {
 	m5_reset_stats();
-        uint8_t  * MMR  = (uint8_t  *)(TOP + 0x00);
-        uint64_t * ARGS = (uint64_t *)(TOP + 0x01);
+        volatile uint8_t  * MMR  = (uint8_t  *)(TOP + 0x0);
+        volatile uint64_t * ARGS = (uint64_t *)(TOP + 0x1);
         // Run Head
         printf("Setting args\n");
         ARGS[0] = feats;
@@ -18,5 +18,6 @@ int main(void) {
 
         m5_dump_stats();
 	m5_exit();
+
         return 0;
 }
