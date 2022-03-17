2d22cd99172d255151da910686ae16b828f6f43b
adityaabhiram3 <adityaabhiram3@gmail.com>
2022-03-18 10:23:21 +1300

    RootCache Working

--- benchmarks/sys_validation/search/hw/search.c
+++ benchmarks/sys_validation/search/hw/search.c
@@ -27,12 +27,33 @@ typedef struct rootCache{
 void top(unsigned * a, unsigned* b, unsigned *c) {
 
 
-	unsigned * f    = (unsigned *)0x80c10000;
+	unsigned * key_rc    = (unsigned *)0x80c10000;
     unsigned * g    = (unsigned *)0x80c10008;
 	// unsigned * m    = (unsigned *)0x80c10010;
     cache_t * h     = (cache_t *)TESTA;
+	int count;
 
-    int count;
+	bTree* cursor;
+	bTree* ref;
+	key_rc[0] = *a;
+    g[1] = *a;
+    g[0] = 1;
+    
+
+    *root = 0x01;
+    while ((*root & 4) != 4) count++;
+
+	// *b=g[0];
+	if(g[0]>-1)
+	{
+		cursor=(bTree*)g[0];
+	}
+	else{
+		cursor=(bTree*)*c;
+	}
+
+	ref=g[0];
+    
 
     // volatile int count = 0;
     // h->occupancy = 0;
@@ -40,169 +61,146 @@ void top(unsigned * a, unsigned* b, unsigned *c) {
 
 	// *b = g[0];
 
-	int rlg=0;
 	int flg=0;
-	int blg=0;
+	int index=0;
 	
-	bTree* cursor = (bTree*)*c;
+	// bTree* cursor = (bTree*)*c;
 	uint64_t key = *a;
 	
 	
 
 	while(!cursor->l){
 		for(int i = 0; i < cursor->n; i++){
-			if(key<cursor->d[i])
-			{
-				rlg=1;
-				blg=i;
-				cursor = cursor->child_ptr[i];
+			if(key<cursor->d[i]){
+				flg=1;
+				index=i;
+				// cursor = cursor->child_ptr[i];
 				
 				break;
 			}
-			if(key == cursor->d[i])
-			{
+			if(key == cursor->d[i]){
+
 				*b=1;
 				flg=1;
-				if(i-1>0)
-				{
-					f[1] = cursor->d[i-1];
+				if(i-1>0){
+					key_rc[1] = cursor->d[i-1];
 					g[1] = key;
-					f[0] = cursor;
+					key_rc[0] = cursor;
 					// m[0] = (unsigned)&key; 
 					g[0] = 2;
 					*root = 0x01;
 					while ((*root & 4) != 4) count++;
 					
 				}
-				else
-				{
-					if(i+1<cursor->n)
-					{
-
-					
-					f[1] = key;
-					g[1] = cursor->d[i+1];
-					f[0] = cursor;
-					// m[0] = (unsigned)&key; 
-					g[0] = 2;
-					*root = 0x01;
-					while ((*root & 4) != 4) count++;
+				else{
+					if(i+1<cursor->n){
+						key_rc[1] = key;
+						g[1] = cursor->d[i+1];
+						key_rc[0] = cursor;
+						// m[0] = (unsigned)&key; 
+						g[0] = 2;
+						*root = 0x01;
+						while ((*root & 4) != 4) count++;
 					}
 					else{
-					f[1] = key;
-					g[1] = cursor->d[i];
-					f[0] = cursor;
-					// m[0] = (unsigned)&key; 
-					g[0] = 2;
-					*root = 0x01;
-					while ((*root & 4) != 4) count++;
+						key_rc[1] = key;
+						g[1] = cursor->d[i];
+						key_rc[0] = cursor;
+						// m[0] = (unsigned)&key; 
+						g[0] = 2;
+						*root = 0x01;
+						while ((*root & 4) != 4) count++;
 					}
 
 				}
-				rlg=1;
-				blg=i;
+				flg=1;
+				index=i;
 
-				cursor = cursor->child_ptr[i];
+				// cursor = cursor->child_ptr[i];
 				break;
 			}
 			if(i == cursor->n - 1){
-				rlg=2;
-				blg=i;
-				cursor = cursor->child_ptr[i+1];
+				flg=2;
+				index=i;
+				// cursor = cursor->child_ptr[i+1];
 				break;
 			}
 		}
 		
-			if(blg-1>0)
-				{
-					f[1] = cursor->d[blg-1];
-					g[1] = cursor->d[blg];
-					f[0] = cursor;
-					// m[0] = (unsigned)&key; 
-					g[0] = 2;
-					*root = 0x01;
-					while ((*root & 4) != 4) count++;
-					
-				}
-				else
-				{
-					if(blg+1<cursor->n)
-					{
-
-					
-					f[1] = key;
-					g[1] = cursor->d[blg+1];
-					f[0] = cursor;
-					// m[0] = (unsigned)&key; 
-					g[0] = 2;
-					*root = 0x01;
-					while ((*root & 4) != 4) count++;
-					}
-					else{
-					f[1] = key;
-					g[1] = cursor->d[blg];
-					f[0] = cursor;
-					// m[0] = (unsigned)&key; 
-					g[0] = 2;
-					*root = 0x01;
-					while ((*root & 4) != 4) count++;
-					}
+		if(index-1>0){
+			key_rc[1] = cursor->d[index-1];
+			g[1] = cursor->d[index];
+			key_rc[0] = cursor;
+			// m[0] = (unsigned)&key; 
+			g[0] = 2;
+			*root = 0x01;
+			while ((*root & 4) != 4) count++;
+			
+		}
+		else{
 		
+			if(index+1<cursor->n){
+				key_rc[1] = key;
+				g[1] = cursor->d[index+1];
+				key_rc[0] = cursor;
+				// m[0] = (unsigned)&key; 
+				g[0] = 2;
+				*root = 0x01;
+				while ((*root & 4) != 4) count++;
+			}
+			else{
+				key_rc[1] = key;
+				g[1] = cursor->d[index];
+				key_rc[0] = cursor;
+				// m[0] = (unsigned)&key; 
+				g[0] = 2;
+				*root = 0x01;
+				while ((*root & 4) != 4) count++;
+			}
+
 
 		}
-			if(rlg==1)
-			cursor=cursor->child_ptr[blg];
-			else if(rlg==2)
-			cursor=cursor->child_ptr[blg+1];
-			else
+		if(flg==1)
+			cursor=cursor->child_ptr[index];
+		else if(flg==2)
+			cursor=cursor->child_ptr[index+1];
+		else
 			cursor=cursor->child_ptr[cursor->n]; 
 
-			rlg=0;
-			blg=0;
-			flg=0;
+		flg=0;
+		index=0;
 	
 	}
-		for(int i = 0; i < cursor->n; i++){
+	for(int i = 0; i < cursor->n; i++){
 		
-		if(key == cursor->d[i])
-		{
+		if(key == cursor->d[i]){
+			*b=1;
 			flg=1;
-			if(i-1>0)
-				{
-					f[1] = cursor->d[i-1];
-					g[1] = key;
-					f[0] = cursor;
-					// m[0] = (unsigned)&key; 
-					g[0] = 2;
-					*root = 0x01;
-					while ((*root & 4) != 4) count++;
-					
-				}
-				else
-				{
+			if(i-1>0){
+				key_rc[1] = cursor->d[i-1];
+				g[1] = key;
+				key_rc[0] = cursor;
+				// m[0] = (unsigned)&key; 
+				g[0] = 2;
+				*root = 0x01;
+				while ((*root & 4) != 4) count++;
+				
+			}
+			else{
+				key_rc[1] = key;
+				g[1] = cursor->d[i+1];
+				key_rc[0] = cursor;
+				// m[0] = (unsigned)&key; 
+				g[0] = 2;
+				*root = 0x01;
+				while ((*root & 4) != 4) count++;
 
-					f[1] = key;
-					g[1] = cursor->d[i+1];
-					f[0] = cursor;
-					// m[0] = (unsigned)&key; 
-					g[0] = 2;
-					*root = 0x01;
-					while ((*root & 4) != 4) count++;
-
-				}
+			}
 		}
 	}
 
-	f[0] = 5;
-    g[1] = 5;
-    g[0] = 1;
-    
-
-    *root = 0x01;
-    while ((*root & 4) != 4) count++;
-
-	*b = g[0];
-
+	
+	// *b = ref;
 
 	return;
 
