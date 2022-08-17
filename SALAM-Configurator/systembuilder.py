b2e2f462983bfdb6549cba40083139ebc889a6cf
Zephan Spencer <zspence1@uncc.edu>
2022-08-16 21:48:56 -0400

    import fixes and refactoring

--- SALAM-Configurator/systembuilder.py
+++ SALAM-Configurator/systembuilder.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 # Import needed packages
-import yaml
-import os
-import textwrap
-import shutil
 import argparse
-from parser import *
+import os
+import shutil
+import config_parser
+
+import yaml
 
 # This requires M5_PATH to point to your gem5-SALAM directory
 M5_Path = os.getenv('M5_PATH')
@@ -27,13 +27,14 @@ def __init__(self, size, options=None):
 \t\tpass\n\n"""
 
 # Parse Arguements
-parser = argparse.ArgumentParser(description="SALAM System Builder")
-parser.add_argument('--sysName', help="Desired gem5 script name", required=True)
-# parser.add_argument('--headerName', help="Header Name", required=True)
-parser.add_argument('--benchDir', help="Path to Benchmark Directory" +
-"Relative to M5_PATH", required=True)
-parser.add_argument('--path', help="Path to M5 Directory", required=False)
-args=parser.parse_args()
+argparser = argparse.ArgumentParser(description="SALAM System Builder")
+argparser.add_argument('--sys-path', help="Path to Benchmark Directory" +
+                       "Relative to M5_PATH", required=True)
+argparser.add_argument(
+    '--sys-name', help="Desired gem5 script name", required=False, default=None)
+argparser.add_argument(
+    '--m5-path', help="Path to M5 Directory", required=False, default=None)
+args = argparser.parse_args()
 
 if M5_Path is None:
     print("Looking for Path Argument from Command Line")
@@ -42,88 +43,102 @@ if M5_Path is None:
         print("M5_PATH Not Found")
         exit(1)
 
-args=parser.parse_args()
+args = argparser.parse_args()
 
 # Set file information
-fileName = args.sysName
-CONFIG_Path = M5_Path + "/configs/SALAM/generated/"
-workingDirectory = M5_Path + "/"  + args.benchDir + "/"
+if args.sys_name == None:
+    file_name = os.path.basename(os.path.normpath(args.sys_path))
+else:
+    file_name = args.sys_name
+Config_Path = M5_Path + "/configs/SALAM/generated/"
+working_directory = M5_Path + "/" + args.sys_path + "/"
+
 
 def main():
     # Set base addresses
-    baseAddress = 0x10020000
-    maxAddress = 0x13ffffff
+    base_address = 0x10020000
+    max_address = 0x13ffffff
     # Load in the YAML file
-    config = openYAML()
+    config = open_yaml()
     # Parse YAML File
-    baseAddress, clusters = parseYAMLFile(config, baseAddress)
+    base_address, clusters = parse_yaml(config, base_address)
     # Generate SALAM Config
-    genConfigFile(clusters)
+    gen_config(clusters)
     # Parse original header for custom code
-    headerList = parseOriginalHeader(clusters)
+    header_list = load_og_header(clusters)
     # Make the header files with custom code
-    genHeaderFiles(headerList, clusters)
+    gen_header(header_list, clusters)
     # Generate full system file
-    shutil.copyfile(M5_Path + "/SALAM-Configurator/fs_template.py", CONFIG_Path + "fs_" + fileName + ".py")
-    f = open(CONFIG_Path + "fs_" + fileName + ".py", "r")
+    shutil.copyfile(M5_Path + "/SALAM-Configurator/fs_template.py",
+                    Config_Path + "fs_" + file_name + ".py")
+    f = open(Config_Path + "fs_" + file_name + ".py", "r")
     fullSystem = f.readlines()
-    fullSystem[65] = "import " + fileName
-    fullSystem[229] = "        " + fileName + ".makeHWAcc(args, test_sys)\n"
-    f = open(CONFIG_Path + "fs_" + fileName + ".py", "w")
+    fullSystem[65] = "import " + file_name
+    fullSystem[229] = "        " + file_name + ".makeHWAcc(args, test_sys)\n"
+    f = open(Config_Path + "fs_" + file_name + ".py", "w")
     f.writelines(fullSystem)
     # Warn if the size is greater than allowed
-    if(clusters[-1].clusterTopAddress>maxAddress):
+    if (clusters[-1].cluster_top_address > max_address):
         print("WARNING: Address range is greater than defined for gem5")
 
-def parseYAMLFile(config, baseAddress):
+
+def parse_yaml(config, base_address, hw_config=None):
     clusters = []
     # Load in each acc cluster and add it to the list
-    for clusterDict in config:
-        for listType,params in clusterDict.items():
-            FOUND_PATH = False
+    for cluster_dict in config:
+        for list_type, params in cluster_dict.items():
+            FOUND_SYS_PATH = False
+            FOUND_HW_PATH = False
             FOUND_DEVICE = False
-            if listType == "acc_cluster":
+            if list_type == "acc_cluster":
                 for param in params:
-                    if "SysPath" or "ConfigPath" in param:
-                        FOUND_PATH = True
-                        config = openYAML(param["SysPath"])
-                        baseAddress, tempCluster = parseYAMLFile(config,baseAddress)
-                        clusters.extend(tempCluster)
+                    if "SysPath" in param:
+                        FOUND_SYS_PATH = True
+                        config = open_yaml(param["SysPath"])
+                        # Recursion Alert!
+                        base_address, temp_cluster = parse_yaml(
+                            config, base_address)
+                        clusters.extend(temp_cluster)
                     elif "HWPath" in param:
-                        FOUND_PATH = True
+                        FOUND_HW_PATH = True
                     else:
                         FOUND_DEVICE = True
 
-        if FOUND_PATH and FOUND_DEVICE:
-            raise Exception("Found device definitions in a cluster with a path to another YAML file.")
-        elif FOUND_PATH:
+        if (FOUND_HW_PATH or FOUND_SYS_PATH) and FOUND_DEVICE:
+            raise Exception(
+                "Found device definitions in a cluster with a path to another YAML file.")
+        elif FOUND_HW_PATH or FOUND_SYS_PATH:
             continue
-        clusterName = None
+        cluster_name = None
         dmas = []
         accs = []
-        for listType,devices in clusterDict.items():
-            if listType == "acc_cluster":
+        for list_type, devices in cluster_dict.items():
+            if list_type == "acc_cluster":
                 for device in devices:
                     if "Name" in device:
-                        clusterName = device['Name']
+                        cluster_name = device['Name']
                     if "DMA" in device:
                         dmas.append(device)
                     if "Accelerator" in device:
                         accs.append(device)
-        clusters.append(AccCluster(clusterName, dmas, accs, baseAddress, M5_Path))
-        baseAddress = clusters[-1].clusterTopAddress + (64 - (int(clusters[-1].clusterTopAddress) % 64))
-        if (int(baseAddress) % 64) != 0:
-            print("Address Alignment Error: " + hex(baseAddress))
-    return baseAddress, clusters
+        clusters.append(config_parser.AccCluster(
+            cluster_name, dmas, accs, base_address, M5_Path))
+        base_address = clusters[-1].top_address + \
+            (64 - (int(clusters[-1].top_address) % 64))
+        if (int(base_address) % 64) != 0:
+            print("Address Alignment Error: " + hex(base_address))
+    return base_address, clusters
 
-def openYAML(path='config.yml'):
-    stream = open(workingDirectory + path, "r")
+
+def open_yaml(path='config.yml'):
+    stream = open(working_directory + path, "r")
     config = yaml.load_all(stream, Loader=yaml.FullLoader)
     return config
 
-def genConfigFile(clusters):
+
+def gen_config(clusters):
     # Write out config file
-    with open(CONFIG_Path + fileName + ".py", 'w') as writer:
+    with open(Config_Path + file_name + ".py", 'w') as writer:
         writer.write(imports)
         writer.write(l1Cache)
         for cluster in clusters:
@@ -140,77 +155,106 @@ def genConfigFile(clusters):
         # Write cluster creation
         writer.write("def makeHWAcc(args, system):\n\n")
         for i in clusters:
-            writer.write("	system." + i.name.lower() + " = AccCluster()" + "\n")
-            writer.write("	build" + i.name + "(args, system, system." + i.name.lower() + ")\n\n")
+            writer.write("	system." + i.name.lower() +
+                         " = AccCluster()" + "\n")
+            writer.write("	build" + i.name +
+                         "(args, system, system." + i.name.lower() + ")\n\n")
 
-def parseOriginalHeader(clusters):
+
+def load_og_header(clusters):
     begin = None
     end = None
     # Read in existing header
-    headerList = []
+    header_list = []
     for i in clusters:
         try:
-            # f = open(workingDirectory + i.name  + "_" + args.headerName + ".h", 'r')
-            f = open(workingDirectory + i.name  + "_hw_defines.h", 'r')
+            # f = open(working_directory + i.name  + "_" + args.headerName + ".h", 'r')
+            f = open(working_directory + i.name + "_hw_defines.h", 'r')
             oldHeader = f.readlines()
-            for i in range(0,len(oldHeader)):
+            for i in range(0, len(oldHeader)):
                 if oldHeader[i] == "//BEGIN GENERATED CODE\n":
                     begin = i
                 elif oldHeader[i] == "//END GENERATED CODE\n" or oldHeader[i] == "//END GENERATED CODE":
                     end = i
             del oldHeader[begin:end+1]
-            headerList.append(oldHeader)
+            header_list.append(oldHeader)
         except:
             print("No Header Found")
             emptyList = []
-            headerList.append(emptyList)
-    return headerList
+            header_list.append(emptyList)
+    return header_list
 
-def genHeaderFiles(headerList, clusters):
+
+def gen_header(header_list, clusters):
     # Write out headers
-    for currentHeader in headerList:
+    for current_header in header_list:
         for cluster in clusters:
-            with open(workingDirectory + cluster.name  + "_hw_defines.h", 'w') as f:
-                currentHeader.append("//BEGIN GENERATED CODE\n")
-                currentHeader.append("//Cluster: " + cluster.name.upper() + "\n")
+            with open(working_directory + cluster.name + "_hw_defines.h", 'w') as f:
+                current_header.append("//BEGIN GENERATED CODE\n")
+                current_header.append(
+                    "//Cluster: " + cluster.name.upper() + "\n")
                 for dma in cluster.dmas:
                     if dma.dmaType == "NonCoherent":
-                        currentHeader.append("//" + dma.dmaType + "DMA" + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_Flags " + hex(dma.address) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_RdAddr " + hex(dma.address + 1) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_WrAddr " + hex(dma.address + 9) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_CopyLen " + hex(dma.address + 17) + "\n")
+                        current_header.append(
+                            "//" + dma.dmaType + "DMA" + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_Flags " + hex(dma.address) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_RdAddr " + hex(dma.address + 1) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_WrAddr " + hex(dma.address + 9) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_CopyLen " + hex(dma.address + 17) + "\n")
                     elif dma.dmaType == "Stream":
-                        currentHeader.append("//" + dma.dmaType + "DMA" + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_Flags " + hex(dma.address) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_RdAddr " + hex(dma.address + 4) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_WrAddr " + hex(dma.address + 12) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_RdFrameSize " + hex(dma.address + 20) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_NumRdFrames " + hex(dma.address + 24) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_RdFrameBufSize " + hex(dma.address + 25) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_WrFrameSize " + hex(dma.address + 26) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_NumWrFrames " + hex(dma.address + 30) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_WrFrameBufSize " + hex(dma.address + 31) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_Stream " + hex(dma.address + 32) + "\n")
-                        currentHeader.append("#define " + dma.name.upper() + "_Status " + hex(dma.statusAddress) + "\n")
+                        current_header.append(
+                            "//" + dma.dmaType + "DMA" + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_Flags " + hex(dma.address) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_RdAddr " + hex(dma.address + 4) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_WrAddr " + hex(dma.address + 12) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_RdFrameSize " + hex(dma.address + 20) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_NumRdFrames " + hex(dma.address + 24) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_RdFrameBufSize " + hex(dma.address + 25) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_WrFrameSize " + hex(dma.address + 26) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_NumWrFrames " + hex(dma.address + 30) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_WrFrameBufSize " + hex(dma.address + 31) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_Stream " + hex(dma.address + 32) + "\n")
+                        current_header.append(
+                            "#define " + dma.name.upper() + "_Status " + hex(dma.statusAddress) + "\n")
                 for acc in cluster.accs:
-                    currentHeader.append("//Accelerator: " + acc.name.upper() + "\n")
-                    currentHeader.append("#define " + acc.name.upper() + " " + hex(acc.address) + "\n")
+                    current_header.append(
+                        "//Accelerator: " + acc.name.upper() + "\n")
+                    current_header.append(
+                        "#define " + acc.name.upper() + " " + hex(acc.address) + "\n")
                     for var in acc.variables:
                         if "Cache" in var.type:
                             continue
                         elif "Stream" in var.type:
-                            currentHeader.append("#define " + var.name + " " + hex(var.address) + "\n")
-                            currentHeader.append("#define " + var.name + "_Status " + hex(var.statusAddress) + "\n")
+                            current_header.append(
+                                "#define " + var.name + " " + hex(var.address) + "\n")
+                            current_header.append(
+                                "#define " + var.name + "_Status " + hex(var.statusAddress) + "\n")
                         else:
-                            currentHeader.append("#define " + var.name + " " + hex(var.address) + "\n")
-                currentHeader.append("//END GENERATED CODE")
-                f.writelines(currentHeader)
-                currentHeader = []
+                            current_header.append(
+                                "#define " + var.name + " " + hex(var.address) + "\n")
+                current_header.append("//END GENERATED CODE")
+                f.writelines(current_header)
+                current_header = []
+
 
 def writeLines(writer, lines):
     for line in lines:
         writer.write("	" + line + "\n")
+
 
 if __name__ == "__main__":
     main()
